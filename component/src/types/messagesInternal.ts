7cdb33988ad1a95638bea1b7afd5ec8f03a78c6c
quietdreamr <fvo@clerk.io>
2024-11-05 15:07:22 +0000

    REVERT

--- component/src/types/messagesInternal.ts
+++ component/src/types/messagesInternal.ts
@@ -4,7 +4,7 @@ import {MessageContent} from './messages';
 
 export type MessageContentI = PropsRequired<MessageContent, 'role'>;
 
-export type UserContentI = {text?: string; files?: {file: File; type: MessageFileType}[]; role?: string};
+export type UserContentI = {text?: string; files?: {file: File; type: MessageFileType}[], role?: string};
 // if message did not overwrite, create a new message
 export interface Overwrite {
   status?: boolean;

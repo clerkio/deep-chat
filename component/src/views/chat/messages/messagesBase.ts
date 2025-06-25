4f12dccd57c17b9c2e0c7ac6f7d0ee85f0d27200
Asger Rise Hansen <arh@clerk.io>
2025-06-25 14:45:56 +0200

    feat: smaller ui changes

--- component/src/views/chat/messages/messagesBase.ts
+++ component/src/views/chat/messages/messagesBase.ts
@@ -1,22 +1,22 @@
-import { MessageElementsStyles, MessageRoleStyles, MessageStyles, UserContent } from '../../../types/messages';
-import { MessageContentI, Overwrite } from '../../../types/messagesInternal';
-import { ProcessedTextToSpeechConfig } from './textToSpeech/textToSpeech';
-import { ElementUtils } from '../../../utils/element/elementUtils';
-import { HTMLDeepChatElements } from './html/htmlDeepChatElements';
-import { LoadingStyle } from '../../../utils/loading/loadingStyle';
-import { RemarkableConfig } from './remarkable/remarkableConfig';
-import { FireEvents } from '../../../utils/events/fireEvents';
-import { LoadingHistory } from './history/loadingHistory';
-import { HTMLClassUtilities } from '../../../types/html';
-import { MessageStyleUtils } from './messageStyleUtils';
-import { IntroPanel } from '../introPanel/introPanel';
-import { Response } from '../../../types/response';
-import { Avatars } from '../../../types/avatars';
-import { MessageUtils } from './messageUtils';
-import { DeepChat } from '../../../deepChat';
-import { Names } from '../../../types/names';
-import { MessageElements } from './messages';
-import { Remarkable } from 'remarkable';
+import {MessageElementsStyles, MessageRoleStyles, MessageStyles, UserContent} from '../../../types/messages';
+import {MessageContentI, Overwrite} from '../../../types/messagesInternal';
+import {ProcessedTextToSpeechConfig} from './textToSpeech/textToSpeech';
+import {ElementUtils} from '../../../utils/element/elementUtils';
+import {HTMLDeepChatElements} from './html/htmlDeepChatElements';
+import {LoadingStyle} from '../../../utils/loading/loadingStyle';
+import {RemarkableConfig} from './remarkable/remarkableConfig';
+import {FireEvents} from '../../../utils/events/fireEvents';
+import {LoadingHistory} from './history/loadingHistory';
+import {HTMLClassUtilities} from '../../../types/html';
+import {MessageStyleUtils} from './messageStyleUtils';
+import {IntroPanel} from '../introPanel/introPanel';
+import {Response} from '../../../types/response';
+import {Avatars} from '../../../types/avatars';
+import {MessageUtils} from './messageUtils';
+import {DeepChat} from '../../../deepChat';
+import {Names} from '../../../types/names';
+import {MessageElements} from './messages';
+import {Remarkable} from 'remarkable';
 
 export class MessagesBase {
   messageElementRefs: MessageElements[] = [];
@@ -138,20 +138,20 @@ export class MessagesBase {
 
   public createMessageElements(text: string, role: string, isTop = false, feedbackValue?: number) {
     const messageElements = MessagesBase.createBaseElements();
-    const { outerContainer, innerContainer, bubbleElement } = messageElements;
+    const {outerContainer, innerContainer, bubbleElement} = messageElements;
     outerContainer.appendChild(innerContainer);
     this.addInnerContainerElements(bubbleElement, text, role);
     MessageUtils.updateRefArr(this.messageElementRefs, messageElements, isTop);
 
     // clerkChat.js feedback
     // We don't want to add chat-feedback if there are 0 messages, as that means only the welcome message is there.
-    if (this.messages.length > 0) {
-      const feedback = document.createElement('chat-feedback');
-      feedback.setAttribute('role', role);
-      feedback.setAttribute('index', (this.messages.length).toString());
-      if (feedbackValue) feedback.setAttribute('feedback', feedbackValue.toString());
-      outerContainer.appendChild(feedback);
-    }
+    // if (this.messages.length > 0) {
+    //   const feedback = document.createElement('chat-feedback');
+    //   feedback.setAttribute('role', role);
+    //   feedback.setAttribute('index', this.messages.length.toString());
+    //   if (feedbackValue) feedback.setAttribute('feedback', feedbackValue.toString());
+    //   outerContainer.appendChild(feedback);
+    // }
 
     return messageElements;
   }
@@ -165,7 +165,7 @@ export class MessagesBase {
     const bubbleElement = document.createElement('div');
     bubbleElement.classList.add('message-bubble');
     innerContainer.appendChild(bubbleElement);
-    return { outerContainer, innerContainer, bubbleElement };
+    return {outerContainer, innerContainer, bubbleElement};
   }
 
   // prettier-ignore
@@ -190,8 +190,8 @@ export class MessagesBase {
 
   public static createMessageContent(content: Response): MessageContentI {
     // it is important to create a new object as its properties get manipulated later on e.g. delete message.html
-    const { text, files, html, _sessionId, role, feedback } = content;
-    const messageContent: MessageContentI = { role: role || MessageUtils.AI_ROLE };
+    const {text, files, html, _sessionId, role, feedback} = content;
+    const messageContent: MessageContentI = {role: role || MessageUtils.AI_ROLE};
     if (text) messageContent.text = text;
     if (files) messageContent.files = files;
     if (html) messageContent.html = html;
